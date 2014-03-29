752dcd65e453ccb77f071c1548c5d155314b45f7
Pedro Henrique (@phinfonet) <pedrohenriqueacruz@gmail.com>
2014-03-29 00:07:50 -0300

    trying to put it to work...

--- pmanager.c
+++ pmanager.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct {
@@ -7,4 +8,16 @@ typedef struct {
 } project;
 
 int main() {
+  char choice;
+  project *p;
+  p = malloc(sizeof (project));
+  printf("Type a to add a project or q to quit: ");
+  scanf("%c", &choice);
+  while (strcmp(&choice, "a") == 0) {
+    printf("Type the project id: ");
+    scanf("%i", &p->id);
+    puts("==========<end>==========");
+    printf("Type a to add a project or q to quit: ");
+    scanf("%c", &choice);
+  }
 }
